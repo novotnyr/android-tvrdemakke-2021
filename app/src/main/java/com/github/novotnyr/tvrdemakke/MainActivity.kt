86e123d45ff668addbea5b8cd727a9d57c6423f1
Robert Novotny <novotnr0@gmail.com>
2021-02-28 16:41:26 +0100

    Pridana obsluha tlacidiel

--- app/src/main/java/com/github/novotnyr/tvrdemakke/MainActivity.kt
+++ app/src/main/java/com/github/novotnyr/tvrdemakke/MainActivity.kt
@@ -2,10 +2,21 @@ package com.github.novotnyr.tvrdemakke
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onYButtonClick(view: View) {
+        Toast.makeText(this, "Klikli ste na ypsilon", Toast.LENGTH_SHORT)
+            .show()
+    }
+    fun onIButtonClick(view: View) {
+        Toast.makeText(this, "Klikli ste na mäkké i", Toast.LENGTH_SHORT)
+            .show()
     }
 }
